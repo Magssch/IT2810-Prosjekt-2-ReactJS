7e6455cbad5a1fd3ecf859fac0837f3171d33057
Viktor Solberg <viktorsolberg@Viktors-MacBook-Pro.local>
2018-09-18 23:37:15 +0200

    Laget komponenter for subheader og tabs. Dette løser issue #7 og #8

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Dice from './Components/Dice';
 import Menu from './Components/Menu';
+import Tabs from './Components/Tabs';
 
 
 class App extends Component {
@@ -23,6 +24,22 @@ class App extends Component {
           <h1 className="title">Lorem Ipsum</h1>
             <Menu/>
         </header>
+          <div>
+              <Tabs>
+                  <div label="Tab1">
+                      Lorem Ipsum
+                  </div>
+                  <div label="Tab2">
+                      dolor sit
+                  </div>
+                  <div label="Tab3">
+                      amet, consectuvet
+                  </div>
+                  <div label="Tab4">
+                      adipiscing elit
+                  </div>
+              </Tabs>
+          </div>
         <div className="container">
           <Dice value={this.state.diceVal}/>
             <button onClick={this.onRoll}>Roll</button>
